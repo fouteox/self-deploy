e0166337e96b561d4cbf3a8ee381ee113dcc8bd1
Florian <fouteox@gmail.com>
2023-12-01 15:59:09 +0100

    Add new features to manage Crons on Server page
    Code for managing Crons in Server page has been expanded to include functionality for creating, validating and displaying cron records. New column definitions and form schema are included for 'user', 'expression', and 'command' fields. This enhances usability and functionality of the module.

--- app/Filament/Resources/ServerResource/Pages/CronServer.php
+++ app/Filament/Resources/ServerResource/Pages/CronServer.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ServerResource\Pages;
 
 use App\Filament\Resources\ServerResource;
+use App\Models\Cron;
+use App\Rules\CronExpression;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRelatedRecords;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 
 class CronServer extends ManageRelatedRecords
 {
@@ -18,4 +26,74 @@ class CronServer extends ManageRelatedRecords
     protected static ?string $title = 'Crons';
 
     protected static ?string $navigationIcon = 'heroicon-s-clock';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user'),
+                TextColumn::make('expression'),
+                TextColumn::make('command'),
+            ])
+            ->filters([
+                // ...
+            ])
+            ->headerActions([
+                $this->customCreateProcess(),
+                //                    ->visible(fn (): bool => $this->getRelationship()->getResults()->count()),
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ])
+            ->emptyStateActions([
+                $this->customCreateProcess(),
+            ]);
+    }
+
+    private function customCreateProcess(): Tables\Actions\CreateAction
+    {
+        return Tables\Actions\CreateAction::make()
+            ->mutateFormDataUsing(function (array $data): array {
+                if (is_null($data['expression'])) {
+                    $data['expression'] = $data['frequency'];
+                }
+
+                unset($data['frequency']);
+
+                return $data;
+            })
+            ->successNotificationTitle(__('The Cron has been created and will be installed on the server.'));
+        //            ->successNotification(null)
+        //            ->after(fn () => Notification::make()
+        //                ->success()
+        //                ->title(__('The Cron has been created and will be installed on the server.'))
+        //                ->broadcast(auth()->user())
+        //            );
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('command')
+                    ->required()
+                    ->maxLength(255)
+                    ->placeholder('php8.2 /home/eddy/site.com/current/artisan schedule:run'),
+                Forms\Components\TextInput::make('user')
+                    ->required()
+                    ->maxLength(255)
+                    ->in(['root', $this->getRecord()->username])
+                    ->default($this->getRecord()->username),
+                Forms\Components\Radio::make('frequency')
+                    ->options(Cron::predefinedFrequencyOptions())
+                    ->required(),
+                Forms\Components\TextInput::make('expression')
+                    ->requiredIf('frequency', 'custom')
+                    ->rule(new CronExpression)
+                    ->maxLength(255),
+            ]);
+    }
 }
