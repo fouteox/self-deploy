96860cf24f922f0986def2aa1ba8e45b35c55867
Florian <fouteox@gmail.com>
2023-12-02 18:47:37 +0100

    "Enhance DatabaseServer functionality with user features"
    Implemented enhancements to DatabaseServer interactions to include user-related capabilities. The original update in the DatabaseServer included additional imports specifically for access to the Database model, Forms, and Notifications. The table method was heightened with record query modifications, an additional TextColumn, and the creation of a new Table action enabling user addition with the necessary form validation. Notifications are sent upon successful user addition. The modification also presents a new form method for required validation on text inputs, thus improving overall usability.

--- app/Filament/Resources/ServerResource/Pages/DatabaseServer.php
+++ app/Filament/Resources/ServerResource/Pages/DatabaseServer.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\ServerResource\Pages;
 
 use App\Filament\Resources\ServerResource;
+use App\Models\Database;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRelatedRecords;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -25,9 +29,11 @@ class DatabaseServer extends ManageRelatedRecords
     public function table(Table $table): Table
     {
         return $table
+            ->modifyQueryUsing(fn ($query) => $query->with('server'))
+            ->recordTitleAttribute('name')
             ->columns([
                 TextColumn::make('name'),
-                TextColumn::make('users')
+                TextColumn::make('users.name')
                     ->listWithLineBreaks()
                     ->limitList(2)
                     ->expandableLimitedList()
@@ -40,13 +46,40 @@ class DatabaseServer extends ManageRelatedRecords
                 Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                // ...
+                Tables\Actions\Action::make('Add user')
+                    ->icon('heroicon-s-clipboard-document')
+                    ->form([
+                        TextInput::make('name')
+                            ->required(),
+                    ])->action(function (Database $database, array $data) {
+
+                        $database->users()->create([
+                            'name' => $data['name'],
+                            'server_id' => $database->server->id,
+                        ]);
+
+                        Notification::make()
+                            ->title('User added')
+                            ->success()
+                            ->send();
+                    }),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 // ...
             ])
             ->emptyStateActions([
                 // ...
+            ]);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
             ]);
     }
 }
