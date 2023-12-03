af5c15fd87f7178ce2e39d9ff9fe209c379dda1a
Florian <fouteox@gmail.com>
2023-12-03 11:32:51 +0100

    Refactor event broadcasting in DaemonUpdated and DaemonDeleted
    Removed dependencies on ProtoneMedia\Splade package in the DaemonUpdated and DaemonDeleted events and replaced it with Laravel's standard broadcasting mechanism. Simplified the usage of Channel and cleanup of code referring to old package. Refactored the broadcastOn() and broadcastWith() methods to use native Laravel broadcasting features. This results in less dependencies and more standard, cleaner code.

--- app/Events/DaemonDeleted.php
+++ app/Events/DaemonDeleted.php
@@ -2,12 +2,12 @@
 
 namespace App\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use ProtoneMedia\Splade\Facades\Splade;
 
 class DaemonDeleted implements ShouldBroadcast
 {
@@ -24,7 +24,7 @@ class DaemonDeleted implements ShouldBroadcast
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
+     * @return array<int, Channel>
      */
     public function broadcastOn(): array
     {
@@ -35,13 +35,11 @@ class DaemonDeleted implements ShouldBroadcast
 
     /**
      * Get the data that should be sent with the broadcasted event.
-     *
-     * @return array|null
      */
-    public function broadcastWith()
+    public function broadcastWith(): ?array
     {
         return [
-            Splade::refreshOnEvent(),
+            //            Splade::refreshOnEvent(),
         ];
     }
 }
