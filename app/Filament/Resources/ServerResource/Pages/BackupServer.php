db9909cdc0160b90d8f1be70370ffda92ad8f29d
Florian <fouteox@gmail.com>
2023-12-03 17:35:03 +0100

    Add table method to BackupServer and import required classes
    In the BackupServer file, a `table` method was added to enable better handling and display of backup details. New imports, such as `Table` and `Builder`, were also included to support this feature. The changes will allow for more effective querying and management of backups in the `BackupServer` class.

--- app/Filament/Resources/ServerResource/Pages/BackupServer.php
+++ app/Filament/Resources/ServerResource/Pages/BackupServer.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\ServerResource;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
 use Filament\Resources\Pages\ManageRelatedRecords;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class BackupServer extends ManageRelatedRecords
 {
@@ -18,4 +20,14 @@ class BackupServer extends ManageRelatedRecords
     protected static ?string $title = 'Backups';
 
     protected static ?string $navigationIcon = 'heroicon-s-rectangle-stack';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->modifyQueryUsing(fn (Builder $query) => $query->with('server'))
+            ->recordTitleAttribute('name')
+            ->columns([
+                // TODO: add backup
+            ]);
+    }
 }
