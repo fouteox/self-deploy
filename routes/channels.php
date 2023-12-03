9c1be80f3f7a6a4489efbf07ec75b4f384e82df2
Florian <fouteox@gmail.com>
2023-12-03 11:51:47 +0100

    Add team broadcast channel in channels.php file
    Imported User model and introduced a new broadcast channel 'teams' in the channels.php file. This channel checks if the current user is a part of the team specified by ID. If yes, it broadcasts the user's and team's ID; otherwise, it returns false. This feature was added to facilitate real-time updates in team-based applications and improve the overall user experience.

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('teams.{id}', function (User $user, $id) {
+    if ($user->currentTeam->id == $id) {
+        return [
+            'user_id' => $user->id,
+            'team_id' => $user->currentTeam->id,
+        ];
+    }
+
+    return false;
 });
