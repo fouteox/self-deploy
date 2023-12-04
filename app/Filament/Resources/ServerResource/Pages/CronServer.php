6064f6d235caf176deea1fa64794b71d87020306
Florian <fouteox@gmail.com>
2023-12-04 14:15:08 +0100

    "Add Server model reference in CronServer class
    Added reference to the Server model in the CronServer class. This commit includes a newly added import statement for the Server model, and a method annotation. Changes were made to improve the associative linkage between Server and CronServer, making the codebase more flexible and understandable for future developments."

--- app/Filament/Resources/ServerResource/Pages/CronServer.php
+++ app/Filament/Resources/ServerResource/Pages/CronServer.php
@@ -7,6 +7,7 @@ use App\Jobs\InstallCron;
 use App\Jobs\UninstallCron;
 use App\Models\ActivityLog;
 use App\Models\Cron;
+use App\Models\Server;
 use App\Rules\CronExpression;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
@@ -22,6 +23,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 
+/* @method Server getRecord() */
 class CronServer extends ManageRelatedRecords
 {
     use BreadcrumbTrait, RedirectsIfProvisioned;
