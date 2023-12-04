9340d198e70b20a1e9e1e13fe747bc8b439b31a8
Florian <fouteox@gmail.com>
2023-12-04 19:11:02 +0100

    Implement 'SoftwareServer' page to display server software
    Added a new 'SoftwareServer' page using the 'Software' model to list installed server software. 'SoftwareServer' makes use of Sushi, a library that allows Eloquent to work with array data and increases performance. A table is initialized to display data on the page.

--- app/Filament/Resources/ServerResource/Pages/SoftwareServer.php
+++ app/Filament/Resources/ServerResource/Pages/SoftwareServer.php
@@ -3,14 +3,22 @@
 namespace App\Filament\Resources\ServerResource\Pages;
 
 use App\Filament\Resources\ServerResource;
+use App\Models\Server;
+use App\Models\Software;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Resources\Pages\Page;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 
-class SoftwareServer extends Page
+/* @method Server getRecord() */
+class SoftwareServer extends Page implements HasTable
 {
-    use BreadcrumbTrait, InteractsWithRecord, RedirectsIfProvisioned {
+    use BreadcrumbTrait, InteractsWithRecord, InteractsWithTable, RedirectsIfProvisioned {
         BreadcrumbTrait::getBreadcrumbs insteadof InteractsWithRecord;
     }
 
@@ -27,5 +35,26 @@ class SoftwareServer extends Page
         $this->record = $this->resolveRecord($record);
 
         static::authorizeResourceAccess();
+    }
+
+    public function table(Table $table): Table
+    {
+        Software::initializeForServer($this->getRecord());
+
+        return $table
+            ->query(Software::query())
+            ->columns([
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                Action::make('restart'),
+            ])
+            ->bulkActions([
+                // ...
+            ])
+            ->paginated(false);
     }
 }
