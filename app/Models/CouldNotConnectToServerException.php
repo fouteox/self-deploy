47a7ed9e2287a095781f53a597a9a17a84dc0f1c
Florian <fouteox@gmail.com>
2023-12-10 21:05:22 +0100

    Add close-modal dispatch event and remove HasActions interface
    Extended the functionality of the cancel button in modal-action blade by adding a 'close-modal' dispatch event. Meanwhile, 'HasActions' interface which was previously implemented in 'SoftwareServer' class has been removed to simplify the class structure.

--- app/Models/CouldNotConnectToServerException.php
+++ app/Models/CouldNotConnectToServerException.php
@@ -6,24 +6,4 @@ use Exception;
 
 class CouldNotConnectToServerException extends Exception
 {
-    public function __construct(
-        private Server $server,
-        string $message = '',
-        int $code = 0,
-        Exception $previous = null,
-    ) {
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * Render the exception.
-     */
-    public function render()
-    {
-        //        Toast::warning(__("Could not connect to the server ':server'", [
-        //            'server' => $this->server->name,
-        //        ]));
-
-        return back(fallback: route('servers.show', $this->server));
-    }
 }
