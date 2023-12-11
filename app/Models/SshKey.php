01f0a3455f5e80058ce48081ab422ebd9a46c973
Florian <fouteox@gmail.com>
2023-12-11 17:51:00 +0100

    Refactor SshKey model
    The SshKey model has been extensively refactored to improve readability and maintainability. HasFactory trait usage was removed and method order rearranged. Improvements have also been made to the use of strings, with 'str_starts_with' now deployed instead of the substr function. The booted method was also refactored to have a return type defined.

--- app/Models/SshKey.php
+++ app/Models/SshKey.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -12,7 +11,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class SshKey extends Model
 {
-    use HasFactory;
     use HasUlids;
 
     protected $casts = [
@@ -23,11 +21,28 @@ class SshKey extends Model
         'name', 'public_key',
     ];
 
-    protected static function booted()
+    protected static function booted(): void
     {
         static::creating(function (SshKey $sshKey) {
             $sshKey->fingerprint = static::generateFingerprint($sshKey->public_key);
         });
+    }
+
+    /**
+     * https://github.com/violuke/rsa-ssh-key-fingerprint
+     */
+    public static function generateFingerprint(string $sshPublicKey, FingerprintAlgorithm $hashAlgorithm = FingerprintAlgorithm::Md5): ?string
+    {
+        if (! str_starts_with($sshPublicKey, 'ssh-')) {
+            return null;
+        }
+
+        $content = explode(' ', $sshPublicKey, 3);
+
+        return match ($hashAlgorithm) {
+            FingerprintAlgorithm::Md5 => implode(':', str_split(md5(base64_decode($content[1])), 2)),
+            FingerprintAlgorithm::Sha256 => base64_encode(hash('sha256', base64_decode($content[1]), true))
+        };
     }
 
     public function user(): BelongsTo
@@ -38,22 +53,5 @@ class SshKey extends Model
     public function getFingerprintAttribute(): string
     {
         return static::generateFingerprint($this->public_key) ?: '';
-    }
-
-    /**
-     * https://github.com/violuke/rsa-ssh-key-fingerprint
-     */
-    public static function generateFingerprint(string $sshPublicKey, FingerprintAlgorithm $hashAlgorithm = FingerprintAlgorithm::Md5): ?string
-    {
-        if (substr($sshPublicKey, 0, 4) != 'ssh-') {
-            return null;
-        }
-
-        $content = explode(' ', $sshPublicKey, 3);
-
-        return match ($hashAlgorithm) {
-            FingerprintAlgorithm::Md5 => implode(':', str_split(md5(base64_decode($content[1])), 2)),
-            FingerprintAlgorithm::Sha256 => base64_encode(hash('sha256', base64_decode($content[1]), true))
-        };
     }
 }
