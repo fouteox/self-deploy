02ce6fc5ad902f4c4bcc528fb636effb45822e0e
Florian <fouteox@gmail.com>
2023-12-13 22:15:15 +0100

    composer update and add highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     content: [
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
+        './resources/views/vendor/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
 }
