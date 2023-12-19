ce9c74c2372735d7a31cdd1b52ce36a65f1530c0
Florian <fouteox@gmail.com>
2023-12-19 13:55:28 +0100

    add broadcasting for ListServers.php

--- app/Filament/Resources/ServerResource/Pages/ListServers.php
+++ app/Filament/Resources/ServerResource/Pages/ListServers.php
@@ -10,6 +10,19 @@ class ListServers extends ListRecords
 {
     protected static string $resource = ServerResource::class;
 
+    public function getListeners(): array
+    {
+        return [
+            'echo-private:teams.'.auth()->user()->current_team_id.',ServerDeleted' => 'refreshComponent',
+            'echo-private:teams.'.auth()->user()->current_team_id.',ServerUpdated' => 'refreshComponent',
+        ];
+    }
+
+    public function refreshComponent(): void
+    {
+        $this->resetTable();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
