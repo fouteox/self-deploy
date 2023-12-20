3d29b521c73399203201bd47afb0d4177a30dc7e
Florian <fouteox@gmail.com>
2023-12-20 13:22:47 +0100

    disable Toast

--- app/Server/Database/CouldNotCreateUserException.php
+++ app/Server/Database/CouldNotCreateUserException.php
@@ -4,7 +4,6 @@ namespace App\Server\Database;
 
 use App\Models\Server;
 use Exception;
-use ProtoneMedia\Splade\Facades\Toast;
 
 class CouldNotCreateUserException extends Exception
 {
@@ -17,9 +16,10 @@ class CouldNotCreateUserException extends Exception
 
     public function render()
     {
-        Toast::warning(__("Could not create the user on server ':server'", [
-            'server' => $this->server->name,
-        ]));
+        // TODO: gerer cette exeception et les autres du même style
+        //        Toast::warning(__("Could not create the user on server ':server'", [
+        //            'server' => $this->server->name,
+        //        ]));
 
         return back(fallback: route('servers.databases.index', $this->server));
     }
