4bbafbb18a57005fa7d2bc1717ed5425945495c5
Florian <fouteox@gmail.com>
2023-12-23 10:54:47 +0100

    refactor LogServer.php

--- app/Filament/Resources/ServerResource/Pages/LogServer.php
+++ app/Filament/Resources/ServerResource/Pages/LogServer.php
@@ -3,14 +3,24 @@
 namespace App\Filament\Resources\ServerResource\Pages;
 
 use App\Filament\Resources\ServerResource;
+use App\Models\Log;
+use App\Models\Server;
+use App\Tasks\GetFile;
 use App\Traits\BreadcrumbTrait;
 use App\Traits\RedirectsIfProvisioned;
+use Filament\Forms\Components\Textarea;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Resources\Pages\Page;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 
-class LogServer extends Page
+/* @method Server getRecord() */
+class LogServer extends Page implements HasTable
 {
-    use BreadcrumbTrait, InteractsWithRecord, RedirectsIfProvisioned {
+    use BreadcrumbTrait, InteractsWithRecord, InteractsWithTable, RedirectsIfProvisioned {
         BreadcrumbTrait::getBreadcrumbs insteadof InteractsWithRecord;
     }
 
@@ -27,5 +37,33 @@ class LogServer extends Page
         $this->record = $this->resolveRecord($record);
 
         static::authorizeResourceAccess();
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Log::queryForLogs($this->getRecord()))
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('description')
+                    ->wrap(),
+            ])
+            ->actions([
+                Action::make('view')
+                    ->button()
+                    ->form([
+                        Textarea::make('fileContents')
+                            ->default(function (Log $record) {
+                                // TODO : ajouter la vérification en cas de fail de chargement du fichier
+                                return $this->getRecord()->runTask(new GetFile($record->id))
+                                    ->asRoot()
+                                    ->throw()
+                                    ->dispatch()
+                                    ->getBuffer();
+                            })
+                            ->autosize(),
+                    ]),
+            ])
+            ->paginated(false);
     }
 }
---
+++ app/Models/Log.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
+
+class Log extends Model
+{
+    use Sushi;
+
+    protected static array $logs = [];
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    public static function queryForLogs(Server $server): Builder
+    {
+        static::$logs = $server->files()->logFiles()->toArray();
+
+        return static::query();
+    }
+
+    public function getRows(): array
+    {
+        return collect(static::$logs)->map(function ($log) {
+            return [
+                'id' => $log->path,
+                'name' => $log->name,
+                'description' => $log->description,
+            ];
+        })->toArray();
+    }
+}
